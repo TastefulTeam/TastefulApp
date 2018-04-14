9ba59b937bed22250fc84cd3b71fd4a60211acc0
Dorian Macias <33275910+dorianinc@users.noreply.github.com>
2018-04-14 12:34:58 -0700

    Add files via upload

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,22 +1,4 @@
 
-$(".background").ready(function () {
-  var slideIndex = 0;
-  carousel();
-
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {
-      slideIndex = 1
-    }
-    x[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 4000); // Change image every 5 seconds
-  }
-});
 
 // Initialize Firebase
 var config = {
@@ -31,47 +13,90 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// Button for adding new user
-$("#submit-button").on("click", function (event) {
-  event.preventDefault();
+//------------------------------------------------//
 
-  //Grab user input
-  var newEmail = $("#email-input").val().trim();
-  var newUserName = $("#user-name-input").val().trim();
-  var newPassword = $("#password-input").val().trim();
-  var newConfirmPassword = $("#confirm-password-input").val().trim();
+function setupCarousel() {
+    var slideIndex = 0;
+    carousel();
 
-  if (newPassword === newConfirmPassword && newPassword.length > 6) {
-    console.log("They Match!");
+    function carousel() {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > x.length) {
+        slideIndex = 1
+      }
+      x[slideIndex - 1].style.display = "block";
+      setTimeout(carousel, 4000); // Change image every 5 seconds
+    }
 
-    var newUserPassword = newPassword
+};
 
-    var user = {
-      email: newEmail,
-      userName: newUserName,
-      password: newUserPassword,
-      foodFeatures: ["littleCato"]
+
+function setupLoginPage() {
+  // Button for adding new user
+  $("#submit-button").on("click", function (event) {
+    event.preventDefault();
+
+    //Grab user input
+    var newEmail = $("#email-input").val().trim();
+    var newUserName = $("#user-name-input").val().trim();
+    var newPassword = $("#password-input").val().trim();
+    var newConfirmPassword = $("#confirm-password-input").val().trim();
+
+    if (newPassword === newConfirmPassword && newPassword.length > 6) {
+      console.log("They Match!");
+
+      var newUserPassword = newPassword
+
+      var user = {
+        email: newEmail,
+        userName: newUserName,
+        password: newUserPassword,
+        foodFeatures: ["littleCato"]
+      };
+
+    } else if (newPassword === newConfirmPassword && newPassword.length < 6) {
+      $("#invalid").empty();
+      $("#invalid").append("Password must be at least 6 characters long.")
+    } else if (newPassword !== newConfirmPassword) {
+      $("#invalid").empty();
+      $("#invalid").append("Looks you're passwords didn't match! Please try again.")
+
     };
-    window.location.href = 'main-page.html';
-  } else if (newPassword === newConfirmPassword && newPassword.length < 6) {
-    $("#invalid").empty();
-    $("#invalid").append("Password must be at least 6 characters long.")
-  } else if (newPassword !== newConfirmPassword) {
-    $("#invalid").empty();
-    $("#invalid").append("Looks you're passwords didn't match! Please try again.")
+    console.log(user);
+    // Uploads Account Info to Firebase database
+    database.ref().push(user);
 
-  };
-  // Uploads Account Info to the database
-  database.ref().push(user);
+    // Clears all of the text-boxes
+    $("#real-name-input").val("");
+    $("#account-name-input").val("");
+    $("#new-password-input").val("");
+    $("#confirm-pasword-input").val("");
 
-  // Clears all of the text-boxes
-  $("#real-name-input").val("");
-  $("#account-name-input").val("");
-  $("#new-password-input").val("");
-  $("#confirm-pasword-input").val("");
+    // Places Account Info into Local Storage
+    localStorage.setItem('localEmail', user.email);
+    localStorage.setItem('localUserName', user.userName);
+    localStorage.setItem('localPassword', user.password);
+    localStorage.setItem('localFoodFeatures', user.foodFeatures);
 
-});
+    console.log("Local Email is:", localEmail);
+    console.log("Local User Name is:", localUserName);
+    console.log("Local password is:", localPassword);
+    console.log("Local food array has:", localFoodFeatures);
 
+  });
+}
+/*
+function setupUserGreeting() {
+  var userName = localStorage.getItem('localUserName');
+  console.log("hello", userName);
+}
+
+/*
 database.ref().on("child_added", function (childSnapShot, prevChildKey) {
   console.log(childSnapShot.val());
 
@@ -87,4 +112,5 @@ database.ref().on("child_added", function (childSnapShot, prevChildKey) {
   console.log("Firebase says your password is: ", fireBasePassword);
   console.log("Firebase says array name is: ", fireBaseFoodFeatures);
 });
+*/
 
