43b55a9c876c6f44d57211c017eaf3134d23726a
Edward Nguyen <ednguyen92@gmail.com>
2018-04-18 23:59:03 -0700

    Fixed Search Box Bug where the autocomplete options show up twice. Also changed radius of search to 4000 and increased amount of restaurants that appear in search

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -1,8 +1,11 @@
 // Global Variables
 var map, places, infoWindow;
 var markers = [];
+// Variable for search bar autocomplete
 var autocomplete;
+// Array of Taste filters
 var food = ["Salty", "Sweet", "Sour", "Fruity", "Vegan", "Crunchy", "Crispy"];
+// Empty array to push taste into keywords
 var chosenFood = [];
 // Currently restrict searchs in autocomplete to USA only
 var countryRestrict = { 'country': 'us' };
@@ -14,6 +17,46 @@ var urlnameRegexp = new RegExp('^https?://.+?/');
 /**************************************************
  *        Start of Map and Marker Code            *
 ***************************************************/
+
+// This function is what generates the map when page is loaded with it starting in the USA
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: { lat: 37.1, lng: -95.7 },
+    panControl: false,
+    zoomControl: false,
+    streetViewControl: false
+  });
+
+  var input = document.getElementById('autocomplete');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  infoWindow = new google.maps.InfoWindow({
+    content: document.getElementById('info-content')
+
+  });
+
+  // Create the autocomplete object based on what the user inputs
+  // Restrict the search to the default USA, and restrict user input to place type "cities".
+  autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(
+      document.getElementById('autocomplete')), {
+      types: ['(cities)'],
+      componentRestrictions: countryRestrict
+    });
+
+  places = new google.maps.places.PlacesService(map);
+
+  autocomplete.addListener('place_changed', onPlaceChanged);
+
+  	var myMarker = new google.maps.Marker({
+		map: map,
+    animation: google.maps.Animation.DROP
+	});
+  yourLocationButton(map, myMarker);
+}
+
+
 function yourLocationButton(map, marker)
 {
   var controlDiv = document.createElement('div');
@@ -75,74 +118,6 @@ function yourLocationButton(map, marker)
   
 }
 
-// This function is what generates the map when page is loaded with it starting in the USA
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: { lat: 37.1, lng: -95.7 },
-    panControl: false,
-    zoomControl: false,
-    streetViewControl: false
-  });
-
-  var input = document.getElementById('autocomplete');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  infoWindow = new google.maps.InfoWindow({
-    content: document.getElementById('info-content')
-
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  }
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-  }
-
-
-  });
-
-  
-
-  // Create the autocomplete object based on what the user inputs
-  // Restrict the search to the default USA, and restrict user input to place type "cities".
-  autocomplete = new google.maps.places.Autocomplete(
-            /** @type {!HTMLInputElement} */(
-      document.getElementById('autocomplete')), {
-      types: ['(cities)'],
-      componentRestrictions: countryRestrict
-    });
-  places = new google.maps.places.PlacesService(map);
-
-  autocomplete.addListener('place_changed', onPlaceChanged);
-  	var myMarker = new google.maps.Marker({
-		map: map,
-    animation: google.maps.Animation.DROP
-	});
-  yourLocationButton(map, myMarker);
-}
 
 
 
@@ -163,11 +138,12 @@ function search() {
   var search = {
     bounds: map.getBounds(),
     types: ['restaurant'],
+    radius: 4000,
     keyword: chosenFood[chosenFood.length-1]
   }
   console.log(search.keyword);
 
-  places.nearbySearch(search, function (results, status) {
+  places.nearbySearch(search, function (results, status, pagination) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
       clearResults();
       clearMarkers();
@@ -188,9 +164,18 @@ function search() {
         google.maps.event.addListener(markers[i], 'click', showInfoWindow);
         setTimeout(dropMarker(i), i * 100);
         addResult(results[i], i);
-        
-
       }
+
+      var getNextPage = null;
+      var moreButton = document.getElementById('moreResults');
+      moreButton.onclick = function() {
+        moreButton.disabled = true;
+        if (getNextPage) getNextPage();
+      };
+      moreButton.disabled = !pagination.hasNextPage;
+      getNextPage = pagination.hasNextPage && function() {
+        pagination.nextPage();
+      };
     }
   });
 }
